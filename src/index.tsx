f134c422ee0ee0db70d97a8090b8cd33b216d0a5
geonil <dean.kim@crypted.co.kr>
2023-08-22 13:56:31 +0900

    first commit

--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,23 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import * as Sentry from '@sentry/react';
+
+Sentry.init({
+    dsn: process.env.REACT_APP_SENTRY_DSN,
+    release: `v0.0.1`,
+    environment: `production`,
+    integrations: [
+        new Sentry.BrowserTracing({
+            tracePropagationTargets: [`localhost`],
+        }),
+        new Sentry.Replay(),
+    ],
+    tracesSampleRate: 1.0,
+    replaysSessionSampleRate: 0.1,
+    replaysOnErrorSampleRate: 1.0,
+});
+
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
